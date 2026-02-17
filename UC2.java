de8136ec167bb3cb912ae4a63c2da08dd4edff84
ReeRee <reehannajeeb129@gmail.com>
2026-02-17 21:01:21 +0530

    Initial commit

---
+++ UC2.java
@@ -0,0 +1,10 @@
+public class UC2 {
+    public static void main() {
+		System.out.println("OOPS IN STAR PATTERN:");
+		System.out.println("*****   ******   *******   ******");
+		System.out.println("*   *   *    *   *     *   *    ");
+		System.out.println("*   *   *    *   *******   ******");
+		System.out.println("*   *   *    *   *              *");
+		System.out.println("*****   ******   *         ******");
+	}
+}
